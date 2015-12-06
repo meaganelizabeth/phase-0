6ffd3fa5716dda3f6982cf6d6d999d31a9d205d8
Meagan Munch <meaganmunch@gmail.com>
2015-12-05 19:11:01 -0500

    Add DBC challenge 4.5

---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,14 @@
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def valid_triangle?(a, b, c)
+  if (a+b) < c || (a+c) < b || (b+c) < a
+  	return false 
+  elsif a == 0 || b == 0 || c == 0
+ 	return false	
+  else 
+  	return true
+  end	
+end
