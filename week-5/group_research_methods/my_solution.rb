3c9518cfaad4114c03291fca24e3627afa09d0d8
Meagan Munch <meaganmunch@gmail.com>
2015-12-10 21:22:27 -0500

    Add DBC challenge 5.4

---
+++ week-5/group_research_methods/my_solution.rb
@@ -0,0 +1,137 @@
+# Research Methods
+
+# I spent 1 hour on this challenge.
+
+i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
+my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
+
+# Person 1's solution
+def my_array_finding_method(source, thing_to_find)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_finding_method(source, thing_to_find)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+# Person 2
+def my_array_modification_method!(array, number)
+  array.map! do |item|
+    if item.is_a? Integer 
+      item += number
+    else
+      item = item
+    end
+  end
+  return array
+end
+
+
+def my_hash_modification_method!(hash, number)
+  hash.each do |key, value|
+    hash[key] = value + number
+  end
+  return hash
+end
+
+# For the array modification method I used .is_a?, which checks to see if an
+# object belongs to a certain class (in this case I used it to find integer
+# values in the array). I also used .map!, which will execute a code block
+# over each item in whatever it's called on and return an array with the
+# modified items. Since .map! only returns arrays, it failed to work on the
+# hash modification method and I ended up using a simple .each block. Though
+# I'm still not excellent at parsing the Ruby Docs, I found looking at
+# examples to give me a better understanding of what each method would
+# achieve.
+
+
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Person 3
+def my_array_sorting_method(source)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_sorting_method(source)
+   source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Person 4
+def my_array_deletion_method!(source, thing_to_delete)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_deletion_method!(source, thing_to_delete)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Person 5
+def my_array_splitting_method(source)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_splitting_method(source, age)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# accountability group how to use the methods.
+#
+#
+#
+
+
+# Release 3: Reflect!
+# What did you learn about researching and explaining your research to others?
+#
+#
+#
+#
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# accountability group how to use the methods.
+#
+#
+#
+
+
+# Release 3: Reflect!
+# What did you learn about researching and explaining your research to others?
+#
+#
+#
+#
