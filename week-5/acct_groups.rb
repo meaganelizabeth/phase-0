bbcddf9585ac32fa7a8a19a3a4ab27dcb3d3e1da
Meagan Munch <meaganmunch@gmail.com>
2015-12-15 13:05:42 -0500

    Add DBC 5.6 edits

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -43,3 +43,38 @@ group(people)
 
 # Reflection
 
+#What was the most interesting and most difficult part of this challenge?
+
+#For me the most interesting and difficult part of this this challenge was
+#determining what an MVP would look like. This challenge was the one that gave
+#me the most trouble this week and I wasn't sure what would be an acceptable
+#submission without rspec to guide my way.
+
+#Do you feel you are improving in your ability to write pseudocode and break
+#the problem down?
+
+#Yes, absolutely. Pseudocode is definately something that I can feel myself
+#getting better at the more I practice it.
+
+#Was your approach for automating this task a good solution? What could have
+#made it even better?
+
+#Again, this was the challenge that gave me the most trouble and I could
+#definately see having a more elegant solution than the one I'm submitting
+#with some more work and a careful inspection of the docs. For now I'm trying
+#to be happy with the working solution I have.
+
+#What data structure did you decide to store the accountability groups in and why?
+
+#I decided to use a hash since it provides for 2 data slots, keys and values,
+#and the accountabilty group numbers as keys and another data structure (an
+#array of names the compose the members of the accounabilty group) as a value.
+
+#What did you learn in the process of refactoring your initial solution? Did
+#you learn any new Ruby methods?
+
+#I did not have time to refactor for this challenge.
+
+
+
+
