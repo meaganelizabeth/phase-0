4fe99ffcf787b7d25772cb86db0177e71e9151da
Meagan Munch <meaganmunch@gmail.com>
2016-01-18 09:24:31 -0600

    Complete inheritence challenge

--- week-9/ruby-review-2/ruby-review.rb
+++ week-9/ruby-review-2/ruby-review.rb
@@ -1,6 +1,7 @@
 # Introduction to Inheritance
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge Marshall Sosland.
+# I spent 3 hours on this challenge
 
 
 # Pseudocode
@@ -16,9 +17,9 @@
 # return phase 1
 # IF week_number is between 13 and 17
 # return phase 2
-# if week_number is greater than 17 
+# if week_number is greater than 17
 # return graduated
-  
+
 #   graduated? method
 #   return currently_in_phase(week_number) == graduated
 
@@ -37,10 +38,9 @@
 #     removes email from email list
 
 
+
 # Initial Solution
-
 # require 'date'
-
 # class GlobalCohort
 #   attr_reader :name, :start_date, :immersive_date, :graduation_date
 #   def initialize(name, start_date)
@@ -54,13 +54,13 @@
 #     date = Date.today
 #     if date < @start_date
 #       puts "This group has not started phase 0 yet"
-#     elsif date >= @start_date && date < @immersive_date
+#     elsif date < @immersive_date
 #       return 0
-#     elsif date >= @immersive_date && date < (@immersive_date + 21)
+#     elsif date < (@immersive_date + 21)
 #       return 1
-#     elsif date >= (@immersive_date + 21) && date < (@immersive_date + 42)
+#     elsif date < (@immersive_date + 42)
 #       return 2
-#     elsif date >= (@immersive_date + 42) && date < @graduation
+#     elsif date < @graduation_date
 #       return 3
 #     else
 #       puts "This group has graduated!"
@@ -69,64 +69,105 @@
 
 #   def graduated?()
 #     date = Date.today
-#     return date > @graduation
+#     return date > @graduation_date
 #   end
 
 # end
 
 # class LocalCohort < GlobalCohort
 #   attr_reader :city
-#   attr_accessor :email_list
-#   attr_accessor :students
-#   def initialize(city)
+#   attr_accessor :email_list, :students
+#   def initialize(name, start_date, city)
+#     super(name, start_date)
 #     @city = city
 #     @email_list = {}
 #     @students = []
 #   end
 
-#   def add_student(student, email)
-#     @students.push(student)
-#     @email_list[student] = email
+#   def add_student(student)
+#     @students.push(student.name)
+#     @email_list[student.name] = student.email
 #   end
 
 #   def remove_student(student)
-#     @students.delete(student)
-#     @email_list.delete(student)
+#     @students.delete(student.name)
+#     @email_list.delete(student.name)
 #   end
 
 #   def num_of_students
 #     @students.length
 #   end
-
 # end
+
+# class Student
+#   attr_reader :name, :email, :city
+#   def initialize(name, email, city)
+#     @name = name
+#     @email = email
+#     @city = city
+#   end
+# end
+
+# puts "-------------------------"
+# puts "testing out initial solution"
+# puts "--------------------------"
+# #Driver code for global cohort
+# #create global, check dates, check phase number, check whether graduated
+# sq = GlobalCohort.new("Squirrels", '09-05-2015')
+# puts sq.start_date
+# puts sq.immersive_date
+# puts sq.graduation_date
+# puts sq.currently_in_phase
+# puts sq.graduated?
+# puts sq.name
+
+# #driver code for local cohort
+# #create local, create 3 students,
+# #add students, display students, remove students, check number of students
+# chi = LocalCohort.new("Squirrels", '09-05-2015',"Chicago")
+# meagan = Student.new("Meagan", "meagan@mail.com", "Chicago")
+# dave = Student.new("Dave", "dave@mail.com", "Chicago")
+# joe = Student.new("Joe", "joe@mail.com", "Chicago")
+# chi.add_student(meagan)
+# chi.add_student(dave)
+# chi.add_student(joe)
+# puts chi.students
+# puts chi.email_list
+# chi.remove_student(joe)
+# puts chi.students
+# puts chi.email_list
+# puts chi.num_of_students
+
+# #inherit dates from global squirrels cohort
+# puts chi.start_date
+# puts chi.immersive_date
+# puts chi.currently_in_phase
+# puts chi.graduated?
+
+
 
 
 # Refactored Solution
-
-require 'date'
-
 class GlobalCohort
-  attr_reader :name, :start_date, :immersive_date, :phase_2, :phase_3, :graduation_date, :today
+  attr_reader :name, :start_date, :immersive_date, :graduation_date
   def initialize(name, start_date)
     @name = name
     @start_date = Date.parse(start_date)
-    @immersive_date = @start_date + 70
-    @phase_2 = @start_date + 91
-    @phase_3 = @start_date + 112
-    @graduation_date = @start_date + 140
-    @today = Date.today
+    @immersive_date = @start_date + 63
+    @graduation_date = @start_date + 126
   end
 
   def currently_in_phase()
-    if @today < @start_date
+    date = Date.today
+    if date < @start_date
       puts "This group has not started phase 0 yet"
-    elsif @today  < @immersive_date
+    elsif date < @immersive_date
       return 0
-    elsif @today < @phase_2
+    elsif date < (@immersive_date + 21)
       return 1
-    elsif @today < @phase_3
+    elsif date < (@immersive_date + 42)
       return 2
-    elsif @today < @graduation_date
+    elsif date < @graduation_date
       return 3
     else
       puts "This group has graduated!"
@@ -134,7 +175,8 @@ class GlobalCohort
   end
 
   def graduated?()
-    return @today > @graduation_date
+    date = Date.today
+    return date > @graduation_date
   end
 
 end
@@ -142,46 +184,93 @@ end
 class LocalCohort < GlobalCohort
   attr_reader :city
   attr_accessor :email_list, :students
-  def initialize(city)
+  def initialize(name, start_date, city)
+    super(name, start_date)
     @city = city
     @email_list = {}
     @students = []
   end
 
-  def add_student(student, email)
-    @students.push(student)
-    @email_list[student] = email
+  def add_student(student)
+    @students.push(student.name)
+    @email_list[student.name] = student.email
   end
 
   def remove_student(student)
-    @students.delete(student)
-    @email_list.delete(student)
+    @students.delete(student.name)
+    @email_list.delete(student.name)
   end
 
   def num_of_students
     @students.length
   end
-
 end
 
-# sq = GlobalCohort.new("Squirrels", '10-11-2015')
-# chi = LocalCohort.new("Chicago")
-# puts sq.start_date
-# puts sq.immersive_date
-# puts sq.graduation_date
-# puts sq.currently_in_phase
-# puts sq.graduated?
-# chi.add_student("Joe", "joe@mail.com")
-# chi.add_student("Meagan", "meagan@mail.com")
-# chi.add_student("Dave", "dave@mail.com")
-# puts chi.students
-# puts chi.email_list
-# chi.remove_student("Joe")
-# puts chi.students
-# puts chi.email_list
+class Student
+  attr_reader :name, :email, :city
+  def initialize(name, email, city)
+    @name = name
+    @email = email
+    @city = city
+  end
+end
 
+puts "-------------------------"
+puts "testing out refactored solution"
+puts "--------------------------"
+#Driver code for global cohort
+#create global, check dates, check phase number, check whether graduated
+sq = GlobalCohort.new("Squirrels", '09-05-2015')
+puts sq.start_date
+puts sq.immersive_date
+puts sq.graduation_date
+puts sq.currently_in_phase
+puts sq.graduated?
+puts sq.name
+
+#driver code for local cohort
+#create local, create 3 students,
+#add students, display students, remove students, check number of students
+chi = LocalCohort.new("Squirrels", '09-05-2015',"Chicago")
+meagan = Student.new("Meagan", "meagan@mail.com", "Chicago")
+dave = Student.new("Dave", "dave@mail.com", "Chicago")
+joe = Student.new("Joe", "joe@mail.com", "Chicago")
+chi.add_student(meagan)
+chi.add_student(dave)
+chi.add_student(joe)
+puts chi.students
+puts chi.email_list
+chi.remove_student(joe)
+puts chi.students
+puts chi.email_list
+puts chi.num_of_students
+
+#inherit dates from global squirrels cohort
+puts chi.start_date
+puts chi.immersive_date
+puts chi.currently_in_phase
+puts chi.graduated?
 
 
 
 
 # Reflection
+
+# What concepts did you review in this challenge?
+
+# In this challenge I reviewed class construction, flow control, inheritance,
+# and date and time in Ruby.
+
+# What is still confusing to you about Ruby?
+
+# For this challenge in particular I had problems getting child classes to
+# inherit properties from their parent classes, which led to some outside
+# research and discovery of the keyword 'super'.
+
+# What are you going to study to get more prepared for Phase 1?
+
+#Ugh, EVERYTHING. If I find the time I'm definately gonna get some more
+#practice writing classes.
+
+
+
